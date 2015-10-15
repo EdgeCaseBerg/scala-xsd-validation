6b52f3b1429796725e5a9397a216fb4d74b8637a
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-10-14 23:30:54 -0400

    add .close to streams for redundancy
    Not neccesary according to the InputSource JavaDoc, but it doesn't hurt
    to be sure.

--- src/main/scala/LoadXmlWithSchema.scala
+++ src/main/scala/LoadXmlWithSchema.scala
@@ -15,7 +15,13 @@ object LoadXmlWithSchema {
 		val sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
 		val s = sf.newSchema(new StreamSource(getClass().getClassLoader().getResourceAsStream(schemaResource)))
 		val in = new FileInputStream(new File(filePath));
-		val is = new InputSource(new InputStreamReader(in));
-		new SchemaAwareFactoryAdapter(s).loadXML(is)
+		val isr = new InputStreamReader(in)
+		val is = new InputSource(isr);
+		try {
+			new SchemaAwareFactoryAdapter(s).loadXML(is)	
+		} finally {
+			isr.close()
+			in.close()
+		}
 	}
 }
